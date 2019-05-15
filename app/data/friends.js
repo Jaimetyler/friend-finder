998375e97fab740c6476de41b978c7f71a6cd7ac
Jaime Tyler <jaimetyler88@gmail.com>
2019-05-15 19:40:37 -0400

    added array of friend objects with paths to photos and random answers

---
+++ app/data/friends.js
@@ -0,0 +1,147 @@
+var friends = [ 
+    {
+        name: "Bill Nye",
+        photo: "https://www.google.com/search?q=bill+nye+photo&safe=off&rlz=1C1CHBF_enUS835US835&source=lnms&tbm=isch&sa=X&ved=0ahUKEwilt97M2J7iAhVKJKwKHfB9AUoQ_AUIECgD&biw=1366&bih=657#imgrc=mtgcF-hYJMFLVM:",
+        scores:[
+            "5",
+            "4",
+            "4",
+            "1",
+            "2",
+            "3",
+            "4",
+            "1",
+            "3",
+            "4"
+        ]
+    },
+    {
+        name: "Judias Buenoano",
+        photo: "https://www.google.com/search?q=judias+buenoano&safe=off&rlz=1C1CHBF_enUS835US835&source=lnms&tbm=isch&sa=X&ved=0ahUKEwis3vHd2J7iAhVDSK0KHV_jDoAQ_AUIDigB&biw=1366&bih=657#imgrc=rXlbhGiD82psFM:",
+        scores:[
+            "2",
+            "3",
+            "4",
+            "3",
+            "5",
+            "1",
+            "2",
+            "3",
+            "2",
+            "5"
+        ]
+    },
+    {
+        name: "Dennis Rodman",
+        photo: "https://www.google.com/search?q=dennis+rodman&safe=off&rlz=1C1CHBF_enUS835US835&source=lnms&tbm=isch&sa=X&ved=0ahUKEwjqyLfp2J7iAhVCXq0KHc6iBgkQ_AUIDygC&biw=1366&bih=657#imgrc=I2c95Pe1aCmAHM:",
+        scores:[
+            "5",
+            "3",
+            "1",
+            "3",
+            "5",
+            "2",
+            "3",
+            "1",
+            "5",
+            "2"
+        ]
+    },
+    {
+        name: "Kim Jung Un",
+        photo: "https://www.google.com/search?q=kim+jong+un&safe=off&rlz=1C1CHBF_enUS835US835&source=lnms&tbm=isch&sa=X&ved=0ahUKEwjGocb82J7iAhVQC6wKHcelDZUQ_AUIDygC&biw=1366&bih=657#imgrc=QvHp-o6bFcwxXM:",
+        scores:[
+            "3",
+            "4",
+            "2",
+            "3",
+            "1",
+            "1",
+            "2",
+            "5",
+            "5",
+            "1"
+        ]
+    },
+    {
+        name: "Aileen Wuonos",
+        photo: "https://www.google.com/search?q=aileen+wuornos&safe=off&rlz=1C1CHBF_enUS835US835&source=lnms&tbm=isch&sa=X&ved=0ahUKEwj-2eOM2Z7iAhVOKa0KHfzIDOQQ_AUIDigB&biw=1366&bih=657#imgrc=VXItaxhCiMPpmM:",
+        scores:[
+            "3",
+            "2",
+            "2",
+            "4",
+            "5",
+            "1",
+            "2",
+            "2",
+            "3",
+            "4"
+        ]
+    },
+    {
+        name: "Amelia Dyer",
+        photo: "https://www.google.com/search?safe=off&rlz=1C1CHBF_enUS835US835&biw=1366&bih=657&tbm=isch&sa=1&ei=xqHcXPS3E6Sf_QaUo6iAAQ&q=Amelia+Dyer&oq=Amelia+Dyer&gs_l=img.3..0j0i67j0l5j0i5i30j0i8i30j0i24.3440.3440..4278...0.0..0.105.105.0j1......1....2j1..gws-wiz-img.dpT9eIONbc8#imgrc=7GFgjJNV42b93M:",
+        scores:[
+            "1",
+            "5",
+            "2",
+            "4",
+            "2",
+            "4",
+            "2",
+            "5",
+            "3",
+            "2"
+        ]
+    },
+    {
+        name: "Ted Bundy",
+        photo: "https://www.google.com/search?q=ted+bundy&safe=off&rlz=1C1CHBF_enUS835US835&source=lnms&tbm=isch&sa=X&ved=0ahUKEwjygOGr2Z7iAhUIVd8KHVDCBxgQ_AUIDigB&biw=1366&bih=657#imgrc=fmaTDagMwy-HSM:",
+        scores:[
+            "3",
+            "4",
+            "1",
+            "5",
+            "1",
+            "3",
+            "4",
+            "2",
+            "1",
+            "4"
+        ]
+    },
+    {
+        name: "John Wayne Gacy",
+        photo: "https://www.google.com/search?q=john+wayne+gacy&safe=off&rlz=1C1CHBF_enUS835US835&source=lnms&tbm=isch&sa=X&ved=0ahUKEwiaoLG62Z7iAhUtmeAKHYj2CxcQ_AUIDigB&biw=1366&bih=657#imgrc=Qzx6VLRE54RrqM:",
+        scores:[
+            "3",
+            "2",
+            "4",
+            "4",
+            "2",
+            "4",
+            "2",
+            "1",
+            "1",
+            "5"
+        ]
+    },
+    {
+        name: "Jeffrey Dahmer",
+        photo: "https://www.google.com/search?q=jeffrey+dahmer&safe=off&rlz=1C1CHBF_enUS835US835&source=lnms&tbm=isch&sa=X&ved=0ahUKEwiBuvHI2Z7iAhXCm-AKHcLgCq4Q_AUIDigB&biw=1366&bih=657#imgrc=7foE37-4wp2VAM:",
+        scores:[
+            "3",
+            "2",
+            "5",
+            "1",
+            "3",
+            "2",
+            "4",
+            "2",
+            "1",
+            "3"
+        ]
+    },
+    
+]
