501e4e8af25e94a29259dbaddb1d7ea650379769
Jaime Tyler <jaimetyler88@gmail.com>
2019-05-15 22:36:16 -0400

    added math to figure difference in user score, and each user in the database

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,8 +1,44 @@
-var friends = require ("../data/friends.js");
+var friends = require("../data/friends.js");
 
-module.exports = function(app) {
+module.exports = function (app) {
     app.get("/api/friends", function (req, res) {
         // console.log(friends)
         res.json(friends);
+
     });
+    
+    app.post("/api/friends", function (req, res) {
+
+        var bestMatch = {
+            name: "",
+            photo: "",
+            friendDifference: 1000
+        };
+
+        console.log(req.body);
+
+        var userInfo = req.body;
+        var userScores = userInfo.scores;
+
+        console.log(userScores);
+
+        var totalDifference = 0;
+
+        for (i = 0; i < friends.length; i++) {
+            console.log(friends[i]);
+            totalDifference = 0;
+
+            for (j = 0; j < friends[i].scores[j]; j++) {
+                totalDifference += Math.abs(paseInt(userScores[j]) - parseInt(friends[i].scores[j]));
+
+                if (totalDifference <= bestMatch.friendDifference) {
+                    bestMatch.name = friends[i].name;
+                    bestMatch.photo = friends[i].photo;
+                    bestMatch.friendDifference = friends[i];
+                }
+            }
+        }
+
+        friends.push(userInfo)
+    })
 }
